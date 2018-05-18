8b693ab5d97c6db87b291632a390b9352fc34f21
Anshuman Chhabra <anshuman.lalakers@gmail.com>
2018-05-18 20:57:29 +0530

    Returning list of graph ops & fixed/extended error atoms

--- lib/tensorflex.ex
+++ lib/tensorflex.ex
@@ -9,28 +9,12 @@ defmodule Tensorflex do
     raise "NIF tf_version/0 not implemented"
   end
 
-  def new_graph do
-    raise "NIF tf_new_graph/0 not implemented"
-  end
-
-  def new_op(_graph, _op, _label) do
-    raise "NIF tf_new_op/3 not implemented"
-  end
-
   def read_graph(_filepath) do
     raise "NIF read_graph/1 not implemented"
   end
 
-  def print_graph_ops(_graph) do
-    raise "NIF print_graph_ops/1 not implemented"
+  def get_graph_ops(_graph) do
+    raise "NIF get_graph_ops/1 not implemented"
   end
-
-  def string_constant(_value) do
-    raise "NIF tf_string_constant/1 not implemented"
-  end
-
-  def create_and_run_sess(_graph, _opdesc, _tensor) do
-    raise "NIF tf_create_and_run_sess/3 not implemented"
-  end
-
+  
 end
