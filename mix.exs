621294d74167f18d5f11a2c4a1ef3bda5a39c5d9
Anshuman Chhabra <anshuman.lalakers@gmail.com>
2018-08-04 16:06:37 +0530

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tensorflex.MixProject do
   def project do
     [
       app: :tensorflex,
-      version: "0.1.0",
+      version: "0.1.2",
       compilers: [:elixir_make] ++ Mix.compilers,
       elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
